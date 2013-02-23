079cbfb6c3ad808b18b3f2d0941800c2ebf8a667
PopPio <pop_sg88@hotmail.com>
2013-02-23 09:32:10 +0700

    build1 (JFrame)
    + Client.java now have JFrame  Hooray!!!

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,10 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 
 public class Client {
 
@@ -6,7 +13,20 @@ public class Client {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		JFrame frame = new JFrame("Tic Tac Toe");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setPreferredSize(new Dimension(800, 600));
 
+		
+		JPanel mainPane = new JPanel();
+		mainPane.setPreferredSize(new Dimension(800, 600));
+		
+		frame.getContentPane().add(mainPane, BorderLayout.CENTER);
+
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
 	}
 
 }
